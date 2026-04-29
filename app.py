b7449b1e423b319557757be0ffb53db7e72c5831
Saeed <saeed144.73@gmail.com>
2026-04-29 19:50:33 +0330

    feat: create register HTML page

--- app.py
+++ app.py
@@ -1,16 +1,53 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "farzan"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+db = SQLAlchemy(app)
 
-@app.route("/api/hello")
-def hello():
-    return jsonify({"message": "Hello MVP"})
+# DATABASE TABLES ============================================================= 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(120))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(200))
+
+
+# APP =========================================================================
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        fullname = request.form.get("fullname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            return "این ایمیل قبلا ثبت شده"
+
+        # هش کردن پسورد
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(
+            fullname=fullname,
+            email=email,
+            password=hashed_password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return "ثبت نام موفق"
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
